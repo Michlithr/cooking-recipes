dd4ef8a185ac1b356f39100a73711f97a4c3ecb4
Michał Litwa <michal.litwa>
2022-02-25 16:23:23 +0100

    added recommended recipes list, recipe card

---
+++ src/components/recipes/RecipeCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="recipe-container">
+    <p>{{ props.recipe }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  recipe?: string;
+}>();
+</script>
+
+<style lang="scss">
+.recipe-container {
+  width: 180px;
+  height: 180px;
+  margin: 20px;
+  border: 1px solid black;
+  box-shadow: 1px black;
+}
+</style>
---
+++ src/components/recipes/RecommendedRecipesList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="list-container">
+    <h1>Recipes propositions</h1>
+    <div class="recipes-container">
+      <RecipeCard
+        v-for="(recipe, key) in recipes"
+        :key="key"
+        :recipe="recipe.title"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import RecipeCard from "@/components/recipes/RecipeCard.vue";
+import { ref } from "vue";
+
+const recipes = ref([
+  {
+    id: 1,
+    title: "title 1",
+  },
+  {
+    id: 2,
+    title: "title 2",
+  },
+  {
+    id: 3,
+    title: "title 3",
+  },
+  {
+    id: 4,
+    title: "title 4",
+  },
+  {
+    id: 5,
+    title: "title 5",
+  },
+  {
+    id: 6,
+    title: "title 5",
+  },
+  {
+    id: 7,
+    title: "title 5",
+  },
+]);
+</script>
+
+<style lang="scss">
+.list-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    margin-bottom: 60px;
+  }
+
+  .recipes-container {
+    width: 60vw;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+}
+</style>
